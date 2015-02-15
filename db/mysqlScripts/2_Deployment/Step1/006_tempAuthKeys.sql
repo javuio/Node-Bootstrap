306b7b81bef64264d5b788c0ba2d5839e00d27c8
Daniel Hindi <danny@cabulous.com>
2015-02-15 09:19:27 -0800

    fix timestamp issue for createdOn columns

--- db/mysqlScripts/2_Deployment/Step1/006_tempAuthKeys.sql
+++ db/mysqlScripts/2_Deployment/Step1/006_tempAuthKeys.sql
@@ -2,7 +2,7 @@
   `key` char(36) NOT NULL,
   email varchar(60) NOT NULL,
   userId int(11) NOT NULL,
-  createdOn datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+  createdOn timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ,
   expiresOn datetime NOT NULL,
   PRIMARY KEY (`key`),
   CONSTRAINT `fk_tempAuthKey_userId_users` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE NO ACTION ON UPDATE NO ACTION
